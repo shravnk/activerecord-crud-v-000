3e7682b2ca2720e5a068ead76e9051b55352120f
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-16 20:51:49 +0000

    automatically backed up by learn

--- db/migrate/20180316204802_create_movies.rb
+++ db/migrate/20180316204802_create_movies.rb
@@ -1,4 +1,11 @@
 class CreateMovies < ActiveRecord::Migration
   def change
+    create_table movies do |t|
+      t.string :title
+      t.string :director
+      t.string :lead
+      t.boolean :in_theatres
+      t.integer :integer
+    end
   end
 end
